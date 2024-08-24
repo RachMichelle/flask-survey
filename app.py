49522835af05f9db5cd10c9e6b8360c46639181d
Rachel <rachel@LAPTOP-2HAP8AE4.localdomain>
2024-08-24 16:53:06 -0400

    complete page and conditional logic

--- app.py
+++ app.py
@@ -19,12 +19,26 @@ def show_homepage():
 @app.route("/questions/<int:question_num>")
 def show_question(question_num):
     """shows question page--page number determined by the length of received responses"""
+
     question_num = len(responses)
     question = survey.questions[question_num]
     return render_template('questions.html', question=question, question_num=question_num)
 
 @app.route("/response", methods=["POST"])
 def add_answer():
+    """posts answer provided by user & adds to responses list. If more questions, redirects to next question. If it has gone through all questions, redirects to completed page"""
+
     answer = request.form["answer"]
     responses.append(answer)
-    return redirect(f'/questions/{len(responses)}')
+
+    if len(responses) == len(survey.questions):
+        return redirect ('/complete')
+    else:
+        return redirect(f'/questions/{len(responses)}')
+    
+@app.route('/complete')
+def show_complete():
+    """Shows thank you page upon completion of the survey"""
+
+    responses.clear()
+    return render_template('complete.html')
